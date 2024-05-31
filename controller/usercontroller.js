46923eb9b89c054d81a7859970076d65ea846acb
dhruvil8484 <dhrvil080@gmail.com>
2024-05-31 09:48:41 +0530

    change this line

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -11,12 +11,12 @@ exports.insert = async (req,res) =>{
 
 exports.get_data = async (req,res) => {
 
-    // var data = await user.find();
+    var data = await user.find();
     // var data = await user.findById('6646c9992e7cdc63e8e26112');
     // var data = await user.find({"email":"admin@gmail.com"});
     // var data = await user.find().count();
     // var data = await user.find().skip(8);
-    var data = await user.find().limit(3);
+    // var data = await user.find().limit(3);
 
     res.status(200).json({
         status:'display data',
